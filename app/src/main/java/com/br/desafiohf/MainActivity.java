f5022857ae030c6adba15807c342a1a7ba7341fd
Luiz Filipe <moresco.luiz@gmail.com>
2017-09-15 19:54:44 -0300

    Implement setup to show items on recyclerView

--- app/src/main/java/com/br/desafiohf/MainActivity.java
+++ app/src/main/java/com/br/desafiohf/MainActivity.java
@@ -1,29 +1,29 @@
 package com.br.desafiohf;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Card> cardsList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private CardListAdapter cardListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        recyclerView = (RecyclerView) findViewById(R.id.cardList);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        cardListAdapter = new CardListAdapter(cardsList);
+        recyclerView.setAdapter(cardListAdapter);
     }
 
 }
