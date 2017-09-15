04fa929b504299e6528c87b14f9a6b0cbd03c772
Luiz Filipe <moresco.luiz@gmail.com>
2017-09-15 19:50:25 -0300

    Create card class

---
+++ app/src/main/java/com/br/desafiohf/Card.java
@@ -0,0 +1,13 @@
+package com.br.desafiohf;
+
+public class Card {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
