117b3c0b5cae99b2b69840266877386a8fcfdce1
Luiz Filipe <moresco.luiz@gmail.com>
2017-09-18 10:27:51 -0300

    Remove line

--- app/src/main/java/com/br/desafiohf/MainActivity.java
+++ app/src/main/java/com/br/desafiohf/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.main, menu);
 
         MenuItem search = menu.findItem(R.id.search);
