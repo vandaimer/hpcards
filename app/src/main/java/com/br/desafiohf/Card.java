f3aaafd0213f368565e632d00f061cc29e2b9966
Luiz Filipe <moresco.luiz@gmail.com>
2017-09-18 00:07:10 -0300

    Add others attrs

--- app/src/main/java/com/br/desafiohf/Card.java
+++ app/src/main/java/com/br/desafiohf/Card.java
@@ -1,11 +1,46 @@
 package com.br.desafiohf;
 
+import java.util.Map;
+
 public class Card {
+
+    private final String NOT_FOUND = "Não informado";
+
     private String name;
     private String image;
+    private String species;
+    private String house;
+    private Map<String, String> wand;
 
     public String getName() {
-        return name;
+        if(!name.isEmpty()) {
+            return name;
+        }
+        return NOT_FOUND;
     }
-    public String getImageURL() { return image; }
+
+    public String getImageURL() {
+        if(!image.isEmpty()) {
+            return image;
+        }
+        return "";
+    }
+
+    public String getSpecies() {
+        if(!species.isEmpty()){
+            return species;
+        }
+        return NOT_FOUND;
+    }
+
+    public String getHouse() {
+        if(!house.isEmpty()) {
+            return house;
+        }
+        return NOT_FOUND;
+    }
+
+    public Map<String, String> getWand() {
+        return wand;
+    }
 }
