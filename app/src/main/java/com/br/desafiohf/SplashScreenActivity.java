771abd8753e83e87ac7895f5bc7b31e0e8add064
Luiz Filipe <moresco.luiz@gmail.com>
2017-09-18 11:47:55 -0300

    Add splash screen

---
+++ app/src/main/java/com/br/desafiohf/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.br.desafiohf;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        goToMain();
+    }
+
+    private void goToMain() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,
+                        MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
