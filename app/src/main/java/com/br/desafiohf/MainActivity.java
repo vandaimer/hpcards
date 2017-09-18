551901b14dc7b16308f4a764dca0b0092296a072
Luiz Filipe <moresco.luiz@gmail.com>
2017-09-18 10:22:47 -0300

    Setup to work the filter list

--- app/src/main/java/com/br/desafiohf/MainActivity.java
+++ app/src/main/java/com/br/desafiohf/MainActivity.java
@@ -1,9 +1,14 @@
 package com.br.desafiohf;
 
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 import com.br.desafiohf.HPApi.HPApiClient;
@@ -27,11 +32,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         recyclerView = (RecyclerView) findViewById(R.id.cardList);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        loadCardsFromProvider();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem search = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
+        search(searchView);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadCardsFromProvider() {
         HPApiClient client = HPApiRequest.getClient();
         Call<List<Card>> call = client.getCharacters();
 
@@ -41,12 +67,28 @@ public class MainActivity extends AppCompatActivity {
                 List<Card> resp = response.body();
                 if(!resp.isEmpty()) {
                     cardsList = new ArrayList<>(resp);
-                    cardListAdapter = new CardListAdapter(cardsList, new CardItemListener(cardsList));
+                    cardListAdapter = new CardListAdapter(cardsList);
                     recyclerView.setAdapter(cardListAdapter);
                 }
             }
             @Override
             public void onFailure(Call<List<Card>> call, Throwable t) {}
+        });
+    }
+
+
+    private void search(SearchView searchView) {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                cardListAdapter.getFilter().filter(newText);
+                return true;
+            }
         });
     }
 }
